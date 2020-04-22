29fc3db23a7ae6198d80751ddc0de68446dab882
fireseed66 <64075652+fireseed66@users.noreply.github.com>
2020-04-22 11:06:27 +0100

    Update fttc2.py
    A welcome message has been added. Also, items such as input boxes and exit messages are now spoken through accessible_output2.

--- fttc2.py
+++ fttc2.py
@@ -1,9 +1,13 @@
 import accessible_output2.outputs.auto
 import sys
+out = accessible_output2.outputs.auto.Auto()
+out.output("Welcome to File Transfer Time Calculator. This tool allows you to estimate how long it will take for data to transfer from one location to another based on data size and transfer speed.")
 def calculate_file_transfer_time():
 	out=accessible_output2.outputs.auto.Auto()
+	out.output("Please type the size of the file you're trying to transfer in megabytes")
 	file_size = float(input("Please type the size of the file you're trying to transfer in megabytes"))
-	transfer_speed = float(input("Please type your internet transfer speed in megabits"))
+	out.output("Please type your data    transfer speed in megabits")
+	transfer_speed = float(input("Please type your data transfer speed in megabits"))
 	speed_in_megabytes = transfer_speed / 8
 	transfer_time = round(file_size / speed_in_megabytes,2)
 	if transfer_time > 86399.99:
@@ -18,5 +22,6 @@ def calculate_file_transfer_time():
 	if try_again == "y":
 		calculate_file_transfer_time()
 	elif try_again == "n":
+		out.output("Thanks for using this tool. For more of my software, please visit https://houseoffireseed.ml/programs")
 		sys.exit("Thanks for using this tool. For more of my software, please visit https://houseoffireseed.ml/programs")
 calculate_file_transfer_time()
